0b42452a3298c043bb421cd238e22eeb62edd1b0
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-09-25 17:27:54 -0400

    update

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def spoiler():
+    return render_template('spoilers.html')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/bootstrap')
 def bootstrap():
     return render_template('signin.html')
-
-
+    
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
